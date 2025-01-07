bec7ac7110a97f3e8ea78e91d4054c64681314a5
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-01-07 14:47:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,11 @@ st.title("Gurias")
 # Leitura dos dados de dengue
 dados = pd.read_csv('dengue_resid_csv_2025.csv', encoding = 'latin1')
 
+# Seleção de ano
+ano = st.selectbox(label = 'Selecione o ano', options = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025])
+
 # Filtro para ano
-filtro_ano = dados['Ano']==2025
+filtro_ano = dados['Ano']==ano
 
 # Aplicar filtro
 dados_ano = dados[filtro_ano]
