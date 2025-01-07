3c85f845c641815e2c005fade00635ba580c5d57
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-01-07 15:05:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import seaborn as sns
 
 # Configuração inicial
 st.set_page_config(page_title="Exemplo gurias", page_icon=":beer:", layout="wide")
@@ -26,3 +27,26 @@ fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
 
 # Mostrar a figura
 st.plotly_chart(fig)
+
+# Fazer com o seaborn
+# Configurar estilo do Seaborn
+sns.set(style="whitegrid")
+
+# Criar o gráfico de barras
+plt.figure(figsize=(8, 6))
+ax = sns.barplot(data=casos_por_ano, x='Ano', y='Confirmados', palette='viridis')
+
+# Adicionar rótulos sobre as barras
+for p in ax.patches:
+    ax.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha='center', va='baseline', fontsize=12, color='black', xytext=(0, 5),
+                textcoords='offset points')
+
+# Configurar título e rótulos
+plt.title('Casos Confirmados de Dengue por Ano', fontsize=16)
+plt.xlabel('Ano', fontsize=12)
+plt.ylabel('Casos Confirmados', fontsize=12)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+
+st.pyplot(ax)
