c2a82a79c87917ffa80197d0c97a855effb287e9
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-01-07 14:35:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 # Configuração inicial
-st.set_page_config(page_title="Meu App", page_icon=":smile:", layout="wide")
+st.set_page_config(page_title="Exemplo gurias", page_icon=":beer:", layout="wide")
 
 # Título da página
-st.title("Meu App")
+st.title("Gurias")
+
+# subtítulo
+st.subheader('Esse é um exemplo')
 
 # Parágrafo
 st.write("Este é um exemplo de aplicativo Streamlit.")
