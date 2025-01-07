f8b6198ee1894586a257fa5b816b469390d5a3d2
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-01-07 14:49:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.title("Gurias")
 dados = pd.read_csv('dengue_resid_csv_2025.csv', encoding = 'latin1')
 
 # Seleção de ano
-ano = st.selectbox(label = 'Selecione o ano', options = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025])
+ano = st.selectbox(label = 'Selecione o ano', options = dados['Ano'].unique())
 
 # Filtro para ano
 filtro_ano = dados['Ano']==ano
