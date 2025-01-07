9e30eed4f2465585c1097647aacd7a25a740f3f1
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-01-07 14:44:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,11 @@ st.title("Gurias")
 # Leitura dos dados de dengue
 dados = pd.read_csv('dengue_resid_csv_2025.csv', encoding = 'latin1')
 
-# Mostrar os dados
-dados
+# Filtro para ano
+filtro_ano = dados['Ano']==2025
+
+# Aplicar filtro
+dados_ano = dados[filtro_ano]
+
+# mostrar dados
+dados_ano
