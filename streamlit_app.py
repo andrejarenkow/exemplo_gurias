bad61288248533cb39eb9c8497e6e20647e39c0c
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-01-07 15:07:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import seaborn as sns
-import matplotlib.pyplot as plt
+
 
 # Configuração inicial
 st.set_page_config(page_title="Exemplo gurias", page_icon=":beer:", layout="wide")
